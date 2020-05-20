37815de6f2e0f2ff2a6cf0c131fd065303157c50
Sonny <pet63910@hotmail.fr>
2020-05-20 09:42:43 +0200

    Exemple simple form

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 React _formik_yup
+                                    10 steps for Formik !!
+
+
+Step 1 -> npm install formik , yup
+Step 2 -> import { Formik } from 'formik;
+Step 3 -> in return :
+                        <Formik>
+                            {() => (
+                                <form>
+
+                                </form>
+                            )}
+                        </Formik>
+Step 4 -> in form : (for each input: mail, name, password...)
+                     <div className="form-group">
+                        <label>Name</label>
+                        <input type="text" className="form-control"></input>
+                    </div>
+Step 5 -> Create function submit = (values) =>{ clg(values) }
+
+Step 6 -> in <Formik onSubmit={ this.submit }>
+
+Step 7 -> Pass object inside 
+    <Formik>
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          isSubmitting
+        })
+Step 8 -> And put for each input:
+
+                <input name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} type="text" className="form-control"></input>
+
+
+Step 9 -> add <form onSubmit={handleSubmit} className="d-flex flex-column p-5 border"> for the preventDefault
+Step 10 -> and stay isSubmitting ! add in <button disabled={isSubmitting} 
+                        for submit only one time. After submit the button became disabled
+Step 11 -> to button add too type="submit"
+Step 1 -> After you need to initial values. for that
+
+     <Formik
+        onSubmit={ this.submit }
+        initialValues={{name:'', email:'', password: ''}}
+      >
+
+Step 1 -> 
+Step 1 -> 
+Step 1 -> 
--- formik_yup/src/App.js
+++ formik_yup/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Formik } from 'formik';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  submit = (values) =>{
+    console.log(values)
+  }
+  render(){
+    return (
+    <div className="App container-fluid d-flex flex-column justify-content-center align-items-center">
+      <Formik
+        onSubmit={ this.submit }
+        initialValues={{name:'', email:'', password: ''}}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          isSubmitting
+        }) => (
+            <form onSubmit={handleSubmit} className="d-flex flex-column p-5 border">
+              <div className="form-group">
+                <label>Name</label>
+                <input name="name" value={values.name} onChange={handleChange} onBlur={handleBlur} type="text" className="form-control"></input>
+              </div>
+              <div className="form-group">
+                <label>Email</label>
+                <input name="email" value={values.email} onChange={handleChange} onBlur={handleBlur}type="email" className="form-control"></input>
+              </div>
+              <div className="form-group">
+                <label>Password</label>
+                <input name="password" value={values.password} onChange={handleChange} onBlur={handleBlur}type="password" className="form-control"></input>
+              </div>
+              <button type="submit" className="btn btn-small btn-primary mt-3" disabled={isSubmitting}>Submit</button>
+            </form>
+        )}
+      </Formik>
     </div>
   );
+  }
+  
 }
 
 export default App;
